55611945bc684de5d5226f041a498d4b6fb620d5
AnaghaAjidev <anaghaajidev@gmail.com>
2026-07-14 14:40:32 +0530

    Navbar and view added

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,24 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddEntry from './components/AddEntry'
+import ViewEntry from './components/ViewEntry'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NavBar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <AddEntry/>
+    <BrowserRouter>
+    <NavBar/>
+    <Routes>
+
+      <Route path='/' element={<AddEntry/>}/>
+      <Route path='/view' element={<ViewEntry/>}/>
+
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
